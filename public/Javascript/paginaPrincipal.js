4a481593c6808025dcd5eff2d151a74c8624641c
Giancarlo <20161333@aloe.ulima.edu.pe>
2019-11-26 01:29:29 -0500

    Camnbio de colores automaticos falta escoger colores
    Falta escoger colores

--- public/Javascript/paginaPrincipal.js
+++ public/Javascript/paginaPrincipal.js
@@ -1,8 +1,30 @@
+var db = firebase.firestore();
 let hoy = new Date()
 
 
 let semanaActual = document.getElementById("fechaSemana")
 let fechaInicio = new Fecha(hoy, "Lunes")
-let fechaFin = new Fecha(hoy, "Sabado")
+let fechaFin = new Fecha(hoy, "Domingo")
+
 semanaActual.innerHTML = `Semana: ${fechaInicio.generarFormatoFechaOriginal()} hasta el ${fechaFin.generarFormatoFechaOriginal()}`;
 
+
+db.collection("turno").where(firebase.firestore.FieldPath.documentId(), ">=", fechaInicio.generarFormatoFechaBD()).where(firebase.firestore.FieldPath.documentId(), "<=", fechaFin.generarFormatoFechaBD()).onSnapshot((querySnapshot) => {
+    querySnapshot.forEach(function(doc) {
+        let casilla = document.getElementById(`${doc.data().dia}-${doc.data().horaInicio}`)
+        if(doc.data().capacidadCubierta == doc.data().capacidadTotal){
+            casilla.setAttribute("bgcolor", "#52ea3b")
+        }
+        else if(doc.data().estado == "Abierto"){
+            casilla.setAttribute("bgcolor", "#0000FF")
+        }
+        else if(doc.data().estado == "Cerrado"){
+            casilla.setAttribute("bgcolor", "#FF0000")
+            casilla.setAttribute("onclick", "")
+            casilla.style.cursor = "auto";
+        }
+        else if(doc.data().estado == "Caducado"){
+            casilla.setAttribute("bgcolor", "#9c9c9c")
+        }
+    });
+});
